7003d6092b39d2f3ae47f66328b88cc3022d1f45
Your Name <you@example.com>
2025-01-06 16:23:38 +0100

    feat: Update default configuration with specific paths and model settings

--- components/config.py
+++ components/config.py
@@ -1,17 +1,17 @@
 DEFAULT_CONFIG = {
     "project_name": "distil-logits",
     "dataset": {
-        "name": None,
+        "name": "/vol/wsdm2024-cot-dataset/shard_0",
         "split": None,
-        "logits_file": None,
-        "num_samples": None,
-        "select_range": None,
+        "logits_file": "/vol/lora_model/distillation_logits-debug.tfrecord",
+        "num_samples": 1,
+        "select_range": (100,101),
     },
     "models": {
-        "teacher": None,
-        "student": None,
-        "student_adapter": None,
-        "teacher_adapter": None,
+        "teacher": "meta-llama/Llama-3.1-70B-Instruct", # only needed if logits_file is not provided
+        "student": "meta-llama/Llama-3.1-8B-Instruct", # The student model to train
+        "student_adapter": "/vol/lora_model/checkpoint-6016",  # Path to the student LoRA adapter
+        "teacher_adapter": "/vol/lora_model/final-checkpoint-200",
         "teacher_vocab_size": None
     },
     "tokenizer": {
@@ -20,7 +20,7 @@ DEFAULT_CONFIG = {
         "pad_token_id": 128001
     },
     "training": {
-        "output_dir": None,
+        "output_dir": "/vol/distilled_model/debug",
         "num_train_epochs": 4,
         "per_device_train_batch_size": 1,
         "gradient_accumulation_steps": 1,
@@ -30,16 +30,16 @@ DEFAULT_CONFIG = {
         "weight_decay": 0.05,
         "warmup_ratio": 0.1,
         "lr_scheduler_type": "cosine",
-        "resume_from_checkpoint": True,
+        "resume_from_checkpoint": None,
         "fp16": False,
-        "bf16": False,
+        "bf16": True,
     },
     "distillation": {
         "temperature": 2.0,
         "alpha": 0.1
     },
     "model_config": {
-        "use_flash_attention": False
+        "use_flash_attention": True
     },
     "lora": {
         "enable_training": True,
@@ -50,5 +50,8 @@ DEFAULT_CONFIG = {
         "task_type": "CAUSAL_LM",
         "target_modules": ["q_proj", "k_proj", "v_proj", "o_proj",
                       "gate_proj", "up_proj", "down_proj",]
+    },
+    "quantization": {
+        "enabled": True,
     }
 }
